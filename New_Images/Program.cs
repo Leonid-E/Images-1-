82d7eef5fbe8ac4df4b36e95bafc6d76febe56f2
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-11-29 12:19:35 +0300

    Создал файл для теста картинок

---
+++ New_Images/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21. Напиcсать программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве. 
+
+Console.WriteLine("Введите координаты первой точки (x, y, z): ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+int Y1 = Convert.ToInt32(Console.ReadLine());
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки (x, y, z): ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+int Y2 = Convert.ToInt32(Console.ReadLine());
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double a, b, c, d;
+a = Math.Pow((X2 - X1), 2);
+b = Math.Pow((Y2 - Y1), 2);
+c = Math.Pow((Z2 - Z1), 2);
+d = Math.Sqrt((a + b + c));
+
+Console.WriteLine($"Расстояние между двумя точками : {Math.Round(d, 2)}");
